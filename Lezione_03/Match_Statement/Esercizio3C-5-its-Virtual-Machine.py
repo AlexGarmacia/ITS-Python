d0ad58cd5b6e719f2512b05161a48ab5774fdb80
AlexGarmacia <alexandrugarmavalerian@gmail.com>
2025-03-09 18:35:12 +0100

    Esercizi

--- Lezione_03/Match_Statement/Esercizio3C-5-its-Virtual-Machine.py
+++
@@ -1,8 +0,0 @@
-utente:dict= {
-nome: str (input("Inserisci il nome:"))
-ruolo: str (input("Inserire il ruolo"))
-età:int=int (input("Inserire l'età"))
-}
-
-
-
--- Lezione_03/Match_Statement/Esercizio3C-5.py
+++
@@ -1,6 +0,0 @@
-utente:dict= {
-"nome": str(input("Inserisci il nome:")),
-"ruolo": str(input("Inserire il ruolo:")),
-"età": int(input("Inserire l'età:"))
-} 
-
