13f1d605f531c462371bd4bf3c33c29f13230705
AlexGarmacia <alexandrugarmavalerian@gmail.com>
2025-03-12 13:39:06 +0100

    Funzioni

--- Lezione_04/Esercizi Funzioni/Esercizio 8-9.py
+++ Lezione_04/Esercizi Funzioni/Esercizio 8-9.py
@@ -1,7 +1,11 @@
-def show_messages(message)
+def show_messages(message):
     for message in messages:
         print(message)
 
 messages = [
     "Ciao! Come va?",
+    "Che bella giornata!"
+    "Oggi piove!"
 ]
+
+show_messages(messages)
