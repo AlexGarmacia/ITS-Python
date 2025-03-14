f7bd4c8443b1badbe96e23a0eadab5f563057c46
AlexGarmacia <alexandrugarmavalerian@gmail.com>
2025-03-14 10:46:45 +0100

    modiche esercizi

--- Lezione_04/Esercizi Funzioni/Esercizio 8-12.py
+++ Lezione_04/Esercizi Funzioni/Esercizio 8-12.py
@@ -1,6 +1,7 @@
-def sandwich(*args)
-    print("il suo sandwich è composto da:")
-    for ingrediente in args
-    print(f"{ingrediente}")
+def sandwich(*args):
+    print("il suo sandwich è composto da: ") 
+    for ingrediente in args:
+         print(f"{ingrediente}")
 
-sandiw
+sandwich("salame","formaggio")
+sandwich("cotoletta","maionese","formaggio","pane")
---
+++ Lezione_04/Esercizi Funzioni/Esercizio 8-14.py
@@ -0,0 +1,7 @@
+def build_profile(marca:str, modello:str, **kwargs): 
+    car=  {
+        "marca":marca;
+        "modello":modello
+    }
+    for chiave, valore in kwargs.items()
+
